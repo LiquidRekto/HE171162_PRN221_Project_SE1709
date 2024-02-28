be9148986fdf93276048e057bb06ce29bf5d7c11
Bui Tien Dung <dungbt2202.forwork@gmail.com>
2024-02-28 23:19:54 +0700

    Init DB connection

---
+++ PRN221_Project_HE171162_SE1709/Models/PRN221_PROJECT_HE171162Context.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PRN221_Project_HE171162_SE1709.Models
+{
+    public partial class PRN221_PROJECT_HE171162Context : DbContext
+    {
+        public PRN221_PROJECT_HE171162Context()
+        {
+        }
+
+        public PRN221_PROJECT_HE171162Context(DbContextOptions<PRN221_PROJECT_HE171162Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("WebCnn"));
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- PRN221_Project_HE171162_SE1709/Program.cs
+++ PRN221_Project_HE171162_SE1709/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PRN221_Project_HE171162_SE1709.Models;
+
 namespace PRN221_Project_HE171162_SE1709
 {
     public class Program
@@ -5,10 +8,14 @@ namespace PRN221_Project_HE171162_SE1709
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<PRN221_PROJECT_HE171162Context>(
+                opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("WebCnn"))
+                );
+
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
+            app.MapRazorPages();
             app.Run();
         }
     }
